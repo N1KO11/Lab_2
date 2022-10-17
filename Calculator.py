f1b280e84bf3334d6b4e2a2d032bb24dc9538fb9
Никита <368322@niuitmo.ru>
2022-10-17 23:20:17 +0300

    test

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    def slozhenie(self, x, y):
+        return x + y
+
+    def umnozhenie(self, x, y):
+        return x * y
+
+    def vichitanie(self, x, y):
+        return x - y
+
+    def delenie(self, x, y):
+        if y != 0:
+            return x / y
